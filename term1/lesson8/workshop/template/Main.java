4909f780f413c0074747320d3adf69eebedb866c
Emilio Mirasola <emilio@mirasola.dk>
2021-02-22 09:08:59 +0100

    git test 2

--- term1/lesson8/workshop/template/Main.java
+++ term1/lesson8/workshop/template/Main.java
@@ -3,7 +3,7 @@ package term1.lesson8.workshop.template;
 import javafx.application.Application;
 
 public class Main {
-//git test
+
 	public static void main(String[] args) {
 		Application.launch(MainApp.class);
 	}
